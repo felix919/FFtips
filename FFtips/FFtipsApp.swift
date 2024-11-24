fb076cacd0e3a35ff745f231387ee0c024901a83
felix919 <panmatsu@gmail.com>
2024-11-25 01:07:08 +0900

    初期Projectを追加した

---
+++ FFtips/FFtipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FFtipsApp.swift
+//  FFtips
+//
+//  Created by 松広登 on 2024/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FFtipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
